91f9ac6a7f5a01599b58b5594b1e368d3feca42e
efemenia <enzofemenia1@gmail.com>
2024-02-10 16:47:46 +0100

    tjr plus

--- ui/static/js/script.js
+++ ui/static/js/script.js
@@ -1,23 +1,52 @@
 const cube = document.querySelector('.cube');
+const pagetitle = document.querySelector('.pagetitle');
+const registerface = document.querySelector('.connectiondata')
+const userdataface = document.querySelector('.userdata')
+const loginface = document.querySelector('.login')
+const fgpassword = document.querySelector('.forgetpassword')
+
+document.querySelector('.backtologin').addEventListener('click', () => {
+    cube.style.animation = "backforgetpassword 1s cubic-bezier(0.68,-0.2,0.265,1) forwards";
+    fgpassword.classList.add('hide')
+    loginface.classList.remove('hide');
+
+
+})
 
 document.querySelector('.back').addEventListener('click', () => {
     cube.style.animation = "return 1s cubic-bezier(0.68,-0.2,0.265,1) forwards";
-    
 })
 
-document.getElementById('register').addEventListener('click', function(event) {
+document.querySelector('.fgpassword').addEventListener('click', () => {
+    console.log('forget password')
+    fgpassword.classList.remove('hide')
+        cube.style.animation = "forgetpassword 1s cubic-bezier(0.68,-0.2,0.265,1) forwards";
+    loginface.classList.add('hide');
+
+});
+
+document.getElementById('register').addEventListener('click', function (event) {
     event.preventDefault();
     cube.style.animation = "turn 1s cubic-bezier(0.68,-0.2,0.265,1) forwards";
 
 });
 
 
-document.getElementById('registerdata').addEventListener('click', function(event) {
+document.getElementById('registerdata').addEventListener('click', function (event) {
     event.preventDefault();
-    const pagetitle = document.querySelector('.pagetitle');
 
-    cube.style.animation = "byecube 2s cubic-bezier(0.68,-0.2,0.265,1) forwards";
+    userdataface.classList.add('hide');
+    registerface.classList.add('hide');
+    loginface.classList.remove('hide');
+
+
+    cube.style.animation = "login 2s cubic-bezier(0.68,-0.2,0.265,1) forwards";
     pagetitle.style.translate = "0px -100px";
+
+    setTimeout(() => {
+        pagetitle.textContent = 'LOGIN';
+        pagetitle.style.translate = "0px 0px";
+    }, 1000);
 
 
     var username = document.getElementById('username').value;
@@ -30,7 +59,7 @@ document.getElementById('registerdata').addEventListener('click', function(event
 
 
     var data = {
-        "id" : 0,
+        "id": 0,
         "username": username,
         "email": email,
         "password": password,
@@ -41,9 +70,9 @@ document.getElementById('registerdata').addEventListener('click', function(event
     };
 
     postData('http://localhost:3003/register', data)
-    .then(data => {
-        console.log("register");
-    });
+        .then(data => {
+            console.log("register");
+        });
 
     function postData(url, data) {
         return fetch(url, {
@@ -53,7 +82,7 @@ document.getElementById('registerdata').addEventListener('click', function(event
             },
             body: JSON.stringify(data)
         })
-        .then(response => response.json());
+            .then(response => response.json());
     }
     console.log("registerdata");
 })
