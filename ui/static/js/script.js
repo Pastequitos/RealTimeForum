eaeaa95271980341d4262c33faa28f77d0752b06
efemenia <enzofemenia1@gmail.com>
2024-02-12 20:44:08 +0100

    oui oui

--- ui/static/js/script.js
+++ ui/static/js/script.js
@@ -18,7 +18,7 @@ document.querySelector('.back').addEventListener('click', () => {
 document.querySelector('.fgpassword').addEventListener('click', () => {
     console.log('forget password')
     fgpassword.classList.remove('hide')
-        cube.style.animation = "forgetpassword 2s cubic-bezier(0.68,-0.2,0.265,1) forwards";
+    cube.style.animation = "forgetpassword 2s cubic-bezier(0.68,-0.2,0.265,1) forwards";
     loginface.classList.add('hide');
 });
 
@@ -32,19 +32,10 @@ document.getElementById('register').addEventListener('click', function (event) {
 document.getElementById('registerdata').addEventListener('click', function (event) {
     event.preventDefault();
 
-    userdataface.classList.add('hide');
-    registerface.classList.add('hide');
-    loginface.classList.remove('hide');
-
-
-    cube.style.animation = "login 2s cubic-bezier(0.68,-0.2,0.265,1) forwards";
-    pagetitle.style.translate = "0px -100px";
-    setTimeout(() => {
-        pagetitle.textContent = 'LOGIN';
-        pagetitle.style.translate = "0px 0px";
-    }, 1000);
-
-
+    
+    
+    
+    
     var username = document.getElementById('username').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
@@ -52,33 +43,52 @@ document.getElementById('registerdata').addEventListener('click', function (even
     var lastname = document.getElementById('lastname').value;
     var age = document.getElementById('age').value;
     var gender = document.getElementById('gender').value;
+    
+    if (!username || !email || !password || !firstname || !lastname || !age || !gender) {
+        cube.style.animation = "errorsignup 0.5s ease-in-out forwards";
+        document.getElementById('signup-required-message').textContent = 'Looks like you missed a field.';
+        return;
 
 
-    var data = {
-        "id": 0,
-        "username": username,
-        "email": email,
-        "password": password,
-        "firstname": firstname,
-        "lastname": lastname,
-        "age": age,
-        "gender": gender
-    };
+    } else {
+        
+        userdataface.classList.add('hide');
+        registerface.classList.add('hide');
+        loginface.classList.remove('hide');
+        cube.style.animation = "login 2s cubic-bezier(0.68,-0.2,0.265,1) forwards";
+        pagetitle.style.translate = "0px -100px";
+        setTimeout(() => {
+            pagetitle.textContent = 'LOGIN';
+            pagetitle.style.translate = "0px 0px";
+        }, 1000);
 
-    postData('http://localhost:3003/register', data)
-        .then(data => {
-            console.log("register");
-        });
+        var data = {
+            "id": 0,
+            "username": username,
+            "email": email,
+            "password": password,
+            "firstname": firstname,
+            "lastname": lastname,
+            "age": age,
+            "gender": gender
+        };
 
-    function postData(url, data) {
-        return fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-            .then(response => response.json());
+        postData('http://localhost:3003/register', data)
+            .then(data => {
+                console.log("register");
+            });
+
+        function postData(url, data) {
+            return fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => response.json());
+        }
+        console.log("registerdata");
     }
-    console.log("registerdata");
+
 })
